eef51121d9c5c363897f29b3ade35101715e7a8e
abhay <abhayc.008@gmail.com>
2020-03-30 21:34:08 +0530

    Changes commite

--- AWS_VS13_Demo/AWS_Helper/AWS_S3_Constant.cs
+++ AWS_VS13_Demo/AWS_Helper/AWS_S3_Constant.cs
@@ -5,8 +5,8 @@ namespace AWS_Helper
 {
     public static class AWS_S3_Constant{
 
-        public static readonly string AccessKeyId = "AKIA5VEKB3TMHMCAJLGR";
-        public static readonly string SecretAccessKey = "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";
+        public static readonly string AccessKeyId = "AKGCCCSFFSFSDFAMCAJLGR";
+        public static readonly string SecretAccessKey = "FoeijABwhefgvhve+iKpqOR0aKhqQqvPgRVi4wNxXb";
         public static readonly RegionEndpoint bucketRegion = RegionEndpoint.USWest2;
     }
 }
